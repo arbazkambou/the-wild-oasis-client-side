8827e8a1f9997c019f5d540b78cf2aca59ea3b1f
Arbaz Shoukat <arbazkamboh342@.com>
2024-07-30 02:25:52 +0500

    fixing reservation bug

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -102,7 +102,7 @@ function ReservationForm({ cabin, user }) {
           <p className="text-primary-300 text-base">Start by selecting dates</p>
           <button
             className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-            disabled={(!startDate && !endDate) || isPending}
+            disabled={!startDate || !endDate || isPending}
           >
             {isPending ? "Reserving..." : "Reserve now"}
           </button>
